ec61e6dd6bd15085ece8ba3e2390731227c2c51c
Filip Chalupa <chalupa.filip@gmail.com>
2020-02-09 15:16:34 +0100

    Basic version

--- README.md
+++ README.md
@@ -7,3 +7,55 @@ Toggles header visibility.
 ```bash
 npm install hiding-header
 ```
+
+## How to use
+
+### HTML:
+
+```html
+<!-- … -->
+<body>
+	<div class="hidingHeader" id="hidingHeader">
+		<div class="hidingHeader-in">
+			<div class="hidingHeader-content">
+				<!-- Your header -->
+			</div>
+		</div>
+	</div>
+	<!-- … -->
+</body>
+<!-- … -->
+```
+
+### CSS:
+
+```css
+.hidingHeader {
+	position: relative;
+	--hidingHeader-height: 0px;
+	--hidingHeader-scrollCap: 0px;
+	z-index: 10;
+	min-height: calc(var(--hidingHeader-scrollCap) + var(--hidingHeader-height));
+	margin-bottom: calc(-1 * var(--hidingHeader-scrollCap));
+	pointer-events: none;
+}
+
+.hidingHeader-in {
+	position: relative;
+	position: sticky;
+	top: 0;
+}
+
+.hidingHeader-content {
+	pointer-events: auto;
+}
+```
+
+### JavaScript:
+
+```javascript
+import { hidingHeader } from 'hiding-header'
+
+const container = document.querySelector('#hidingHeader')
+hidingHeader(container)
+```
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,58 @@
 export function hidingHeader(container: HTMLElement) {
-	console.warn('Not implemented.')
+	const DEFAULT_CONTENT_SELECTOR = '*'
+	const DEFAULT_HEIGHT_PROPERTY_NAME = '--hidingHeader-height'
+	const DEFAULT_SCROLL_CAP_PROPERTY_NAME = '--hidingHeader-scrollCap'
+
+	const contentSelector = DEFAULT_CONTENT_SELECTOR
+	const heightPropertyName = DEFAULT_HEIGHT_PROPERTY_NAME
+	const scrollCapPropertyName = DEFAULT_SCROLL_CAP_PROPERTY_NAME
+
+	let lastScrollTop = 0
+	let contentHeight = 0
+	let wasScrollingDown = true
+	let lastScrollCap = 0
+
+	const content = container.querySelector(contentSelector)
+	if (content === null) {
+		throw new Error(`Content '${contentSelector}' not found in container.`)
+	}
+
+	const getContentHeight = () => {
+		return content.clientHeight
+	}
+
+	const onScroll = () => {
+		const currentContentHeight = getContentHeight() // @TODO: throttle/cache
+		if (contentHeight !== currentContentHeight) {
+			contentHeight = currentContentHeight
+			container.style.setProperty(heightPropertyName, `${contentHeight}px`)
+		}
+
+		const scrollTop = window.scrollY
+		const isScrollingDown = scrollTop > lastScrollTop
+		if (isScrollingDown !== wasScrollingDown) {
+			const scrollCap = isScrollingDown
+				? scrollTop
+				: Math.max(0, scrollTop - contentHeight)
+
+			// @TODO: handle changes in scroll direction if header is partially visible
+
+			if (scrollCap !== lastScrollCap) {
+				wasScrollingDown = isScrollingDown
+				container.style.setProperty(scrollCapPropertyName, `${scrollCap}px`)
+				lastScrollCap = scrollCap
+			}
+		}
+		lastScrollTop = scrollTop
+	}
+
+	const onResize = onScroll
+
+	const initialize = () => {
+		window.addEventListener('scroll', onScroll)
+		window.addEventListener('resize', onResize)
+		onScroll()
+	}
+
+	initialize()
 }
