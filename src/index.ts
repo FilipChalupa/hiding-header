79e0b9cc950b1b52d7097efe21df2b6033ac3b06
Filip Chalupa <chalupa.filip@gmail.com>
2021-09-29 14:24:58 +0200

    Add v2 base

--- src/index.ts
+++ src/index.ts
@@ -1,282 +1,52 @@
 export interface HidingHeaderOptions {
-	heightPropertyName?: string
-	boundsHeightPropertyName?: string
-	animationOffsetPropertyName?: string
-	snap?: boolean
-	onHeightChange?: (height: number) => void
-	onVisibleHeightChange?: (height: number) => void
-	onHomeChange?: (isHome: boolean) => void
+	snap?: boolean // Reveal or hide header if user stops scrolling in middle
+	onHeightChange?: (height: number) => void // When content height changes
+	onVisibleHeightChange?: (visibleHeight: number) => {} // When part of header is revealed
+	onIsAtInitialPositionChange?: (isAtInitialPosition: boolean) => void // When returns to or leaves initial position
 }
 
-export function hidingHeader(
-	container: HTMLElement,
-	{
-		heightPropertyName = '--hidingHeader-height',
-		boundsHeightPropertyName = '--hidingHeader-bounds-height',
-		animationOffsetPropertyName = '--hidingHeader-animation-offset',
+export const hidingHeader = (
+	containerElement: HTMLElement,
+	options: HidingHeaderOptions = {}
+) => {
+	const {
 		snap = true,
 		onHeightChange = () => {},
 		onVisibleHeightChange = () => {},
-		onHomeChange = () => {},
-	}: HidingHeaderOptions = {}
-) {
-	const content = container.querySelector('*') as HTMLDivElement
+		onIsAtInitialPositionChange = () => {},
+	} = options
 
-	let lastScrollTopPosition = 0
-
-	let contentHeight = (() => {
-		const getContentHeight = () => content.clientHeight
-		const setContentHeightProperty = (height: number) => {
-			container.style.setProperty(heightPropertyName, `${height}px`)
-		}
-
-		const resizeObserver = new ResizeObserver(() => {
-			const newContentHeight = getContentHeight()
-			if (contentHeight !== newContentHeight) {
-				contentHeight = newContentHeight
-				setContentHeightProperty(contentHeight)
-				onScroll()
-				onHeightChange(contentHeight)
-			}
-		})
-		resizeObserver.observe(content)
-
-		const initialHeight = getContentHeight()
-		setContentHeightProperty(initialHeight)
-		return initialHeight
-	})()
-
-	let visibleHeight = contentHeight
-
-	let paused = false
-	let home = true
-	let lastBoundsHeight = 0
-	let pointersDown = 0
-	const parent = container.parentElement
-	if (parent === null) {
-		throw new Error('Missing parent element.')
-	}
-
-	const getParentHeight = () => {
-		return parent.clientHeight
-	}
-
-	const getGlobalTopOffset = () => {
-		return container.offsetTop
-	}
-
-	const getRelativeTopOffset = () => {
-		return getGlobalTopOffset() - (container.parentElement?.offsetTop || 0)
-	}
-
-	const animateOffset = (initialOffset: number) => {
-		if (initialOffset === 0) {
-			return // Nothing to animate
-		}
-
-		content.style.transition = 'none'
-		container.style.setProperty(
-			animationOffsetPropertyName,
-			`${initialOffset}px`
-		)
-
-		content.offsetHeight // Trigger transition
-
-		content.style.transition = ''
-		container.style.setProperty(animationOffsetPropertyName, '0px')
-	}
-
-	const updateBoundsHeight = (boundsHeight: number) => {
-		if (boundsHeight !== lastBoundsHeight) {
-			container.style.setProperty(boundsHeightPropertyName, `${boundsHeight}px`)
-			lastBoundsHeight = boundsHeight
-		}
-	}
-
-	const capBoundsHeight = (rawBoundsHeight: number) =>
-		Math.min(
-			getParentHeight() - getRelativeTopOffset(),
-			Math.max(contentHeight, rawBoundsHeight)
-		)
-
-	const snapIfPossible = () => {
-		if (snap && visibleHeight !== 0 && visibleHeight !== contentHeight) {
-			if (visibleHeight < contentHeight / 2) {
-				hide()
-			} else {
-				reveal()
-			}
-		}
-	}
-
-	const onScrollStopDebounced = (() => {
-		let timer: number
-
-		return () => {
-			window.clearTimeout(timer)
-			timer = window.setTimeout(() => {
-				if (snap && pointersDown === 0) {
-					snapIfPossible()
-				}
-			}, 100)
-		}
-	})()
-
-	const updateVisibleHeight = () => {
-		visibleHeight = (() => {
-			const { top } = content.getBoundingClientRect()
-			const newVisibleHeight = Math.max(
-				0,
-				Math.min(contentHeight + top, contentHeight)
-			)
-
-			if (visibleHeight !== newVisibleHeight) {
-				onVisibleHeightChange(newVisibleHeight)
-			}
-
-			return newVisibleHeight
-		})()
-	}
-
-	const updateHome = () => {
-		home = (() => {
-			const newHome = contentHeight >= lastBoundsHeight
-
-			if (home !== newHome) {
-				onHomeChange(newHome)
-			}
-
-			return newHome
-		})()
-	}
-
-	const onTransitioning = () => {
-		updateVisibleHeight()
-	}
-
-	;(() => {
-		let running = false
-		const loop = () => {
-			if (!running) {
-				return
-			}
-			onTransitioning()
-			requestAnimationFrame(loop)
-		}
-		content.addEventListener('transitionstart', () => {
-			running = true
-			loop()
-		})
-		content.addEventListener('transitionend', () => {
-			running = false
-			onTransitioning()
-		})
-	})()
-
-	const onScroll = () => {
-		const globalTopOffset = getGlobalTopOffset()
-
-		// Handle bounds height
-		const scrollTopPosition = window.scrollY
-		const isScrollingDown = scrollTopPosition > lastScrollTopPosition
-
-		if (!paused) {
-			const boundsHeight = capBoundsHeight(
-				(() => {
-					if (isScrollingDown) {
-						const newBoundsHeight = scrollTopPosition - globalTopOffset
-						if (lastBoundsHeight < newBoundsHeight) {
-							return newBoundsHeight
-						}
-						return lastBoundsHeight
-					} else {
-						const newBoundsHeight =
-							scrollTopPosition - globalTopOffset + contentHeight
-						if (lastBoundsHeight > newBoundsHeight) {
-							return newBoundsHeight
-						}
-						return lastBoundsHeight
-					}
-				})()
-			)
-
-			updateBoundsHeight(boundsHeight)
-
-			onScrollStopDebounced()
-
-			updateVisibleHeight()
-
-			updateHome()
-		}
-
-		lastScrollTopPosition = scrollTopPosition
-	}
-
-	const onResize = onScroll
-
-	document.addEventListener('pointerdown', () => {
-		pointersDown++
-		onScroll()
-	})
-
-	document.addEventListener('pointerup', () => {
-		pointersDown--
-		onScroll()
-	})
-
-	const initialize = () => {
-		window.addEventListener('scroll', onScroll)
-		window.addEventListener('resize', onResize)
-		onScroll()
-	}
-
-	initialize()
-
-	const run = () => {
-		paused = false
-	}
-	const pause = () => {
-		paused = true
-	}
-	const isPaused = () => {
-		return paused
-	}
-	const isHome = () => home
+	const contentElement = containerElement.querySelector('*') // Direct descendant
 
 	const reveal = () => {
-		const scrollTopPosition = window.scrollY
-
-		const globalTopOffset = getGlobalTopOffset()
-
-		const boundsHeight = capBoundsHeight(
-			scrollTopPosition - globalTopOffset + contentHeight
-		)
-
-		animateOffset(lastBoundsHeight - boundsHeight)
-		updateBoundsHeight(boundsHeight)
+		console.log('@TODO')
 	}
 
 	const hide = () => {
-		const scrollTopPosition = window.scrollY
-		const globalTopOffset = getGlobalTopOffset()
-
-		const boundsHeight = capBoundsHeight(scrollTopPosition - globalTopOffset)
-
-		animateOffset(lastBoundsHeight - boundsHeight)
-		updateBoundsHeight(boundsHeight)
+		console.log('@TODO')
 	}
 
-	const getHeight = () => contentHeight
-	const getVisibleHeight = () => visibleHeight
+	const height = () => {
+		console.log('@TODO')
+	}
+
+	const visibleHeight = () => {
+		console.log('@TODO')
+	}
+
+	const isAtInitialPosition = () => {
+		console.log('@TODO')
+	}
+
+	const keepRevealed = (revealed = true) => {
+		console.log('@TODO')
+	}
 
 	return {
-		run,
-		pause,
-		isPaused,
-		isHome,
 		reveal,
 		hide,
-		getHeight,
-		getVisibleHeight,
+		height,
+		visibleHeight,
+		isAtInitialPosition,
 	}
 }
